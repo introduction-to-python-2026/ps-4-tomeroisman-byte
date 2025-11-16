e9af21c945d563f9ac164187d7065af0f38cdd00
sleepy_tomer <tomeroisman@gmail.com>
2025-11-16 10:03:59 +0200

    Add split_before_each_uppercases function
    Added a function to split a string before each uppercase letter.

--- string_utils.py
+++ string_utils.py
@@ -1,3 +1,18 @@
+def split_before_each_uppercases(formula):
+    parts = []
+    current = ""
+    for ch in formula:
+        if ch.isupper():
+            if current:
+                parts.append(current)
+            current = ch  # start new chunk
+        else:
+            current += ch
+    if current:
+        parts.append(current)
+    return parts
+
+
 def split_at_digit(formula):
     prefix = ""
     num = ""
@@ -14,17 +29,3 @@ def split_at_digit(formula):
         return prefix, int(num)
     else:
             return formula, 1
-            
-def split_before_each_uppercases(formula):
-    parts = []
-    current = ""
-    for ch in formula:
-        if ch.isupper():
-            if current:
-                parts.append(current)
-            current = ch  # start new chunk
-        else:
-            current += ch
-    if current:
-        parts.append(current)
-    return parts
