5c6c9dca4d50aa1d2fb50970a27a87e21e4ca1b6
sleepy_tomer <tomeroisman@gmail.com>
2025-11-16 09:23:42 +0200

    Update string_utils.py

--- string_utils.py
+++ string_utils.py
@@ -1,17 +1,20 @@
-def split_before_each_uppercases(formula):
-    parts = []
-    current = ""
-    for ch in formula:
-        if ch.isupper():
-            if current:
-                parts.append(current)
-            current = ch  # start new chunk
+def split_at_digit(formula):
+    prefix = ""
+    num = ""
+    for i in formula:
+        if i.isdigit:            
+            num += i
         else:
-            current += ch
-    if current:
-        parts.append(current)
-    return parts
-def split_before_each_uppercase(formula):
+            if number:
+                break
+            break
+            prefix += i
+        if num:
+            return prefix, int(num)
+        else:
+            return formula, 1
+            
+def split_before_each_uppercases(formula):
     parts = []
     current = ""
     for ch in formula:
